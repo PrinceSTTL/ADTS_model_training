003f81819a9fed923fe1739f2cc1e11ac3d83ff6
khushbumadhani <104305089+khushbumadhani@users.noreply.github.com>
2024-07-10 11:08:54 +0530

    folder structure changes

--- Four_Wheeler_Training/Car_segmentation/Training/train.py
+++ Four_Wheeler_Training/Car_segmentation/Training/train.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")
 
 
-results = model.train(data="..\Data\Annotated_Data\version-1-car-segmentation\data.yaml", epochs=100, imgsz=640 , conf=0.5)
+results = model.train(data="..\Data\Annotated_Data\data.yaml", epochs=100, imgsz=640 , conf=0.5)
 model.save("../Models/model.pt")
--- Four_Wheeler_Training/Seat_belt_detection/Training/train.py
+++ Four_Wheeler_Training/Seat_belt_detection/Training/train.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")
 
-results = model.train(data="..\data\Annotated_Data\Seatbelt-detection.v1i.yolov8\data.yaml", epochs=100, imgsz=640 , conf=0.5)
+results = model.train(data="..\data\Annotated_Data\data.yaml", epochs=100, imgsz=640 , conf=0.5)
 model.save("../Models/model.pt")
